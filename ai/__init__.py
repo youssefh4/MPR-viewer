170f0b4018eab800cae5fecee0cafeb13af9a854
youssef hisham <youssefhisham366@gmail.com>
2025-10-18 12:32:08 +0300

    Delete ai directory

--- ai/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AI module for medical image orientation detection.
-"""
-
-from .orientation import detect_orientation_ai, OrientationResNet18
-
-__all__ = ['detect_orientation_ai', 'OrientationResNet18']
