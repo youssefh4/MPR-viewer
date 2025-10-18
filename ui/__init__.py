5abc9563260ce8fff23b7ece9ec3a1424498c45e
youssef hisham <youssefhisham366@gmail.com>
2025-10-18 12:32:25 +0300

    Delete ui directory

--- ui/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-UI components for the medical viewer.
-"""
-
-from .slice_view import SliceView
-from .collapsible_box import CollapsibleBox
-from .dialogs import SliceRangeDialog
-
-__all__ = ['SliceView', 'CollapsibleBox', 'SliceRangeDialog']
